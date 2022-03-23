422f87e28046f7d78703ca0d02a54729d4b89b66
Allen Shen <allenshen1112@gmail.com>
2022-03-23 15:46:08 -0400

    Initial commit

---
+++ src/SigmaFile.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+class SigmaFileSystem {
+
+  public SigmaFileSystem() {
+  }
+
+  // Feel free to modify the parameter/return types of these functions
+  // as you see fit. Please add comments to indicate your changes with a
+  // brief explanation. We care more about your thought process than your
+  // adherence to a rigid structure.
+
+  int getTotalDashboards() {
+    // TODO: implement
+    return 0;
+  }
+
+  int getTotalWorksheets() {
+    // TODO: implement
+    return 0;
+  }
+
+  void addNewFile(String fileName, String fileType, int folderId) {
+    // TODO: implement
+    return;
+  }
+
+  int getFileId(String fileName, int folderId) {
+    // TODO: implement
+    return 0;
+  }
+
+  void moveFile(int fileId, int newFolderId) {
+    // TODO: implement
+    return;
+  }
+
+  String[] getFiles(int folderId) {
+    // TODO: implement
+    return new String[0];
+  }
+
+  void printFiles() {
+    // TODO: implement
+    return;
+  }
+
+}
+
+
+
+
+
+
+public class SigmaFile {
+  public static void main(String[] args) {
+    System.out.println("please work");
+  }
+}
