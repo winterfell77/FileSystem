0f324e1c030e775a24532b8e040300e0b19133f5
Allen Shen <allenshen1112@gmail.com>
2022-03-24 13:25:54 -0400

    finals

--- src/SigmaFile.java
+++ src/SigmaFile.java
@@ -1,17 +1,19 @@
+/*
+ * Click `Run` in the top left corner to run the command line program. Output will appear in the "Program Output" tab in the right pane.
+ */
+
 import java.io.*;
 import java.util.*;
-
 abstract class files{
   String name;
   int uniqueId;
-  files parent;
+  files parent; //parent file
   Boolean isfolder;
 
   public files(String name, int uniqueId){
     this.name = name;
     this.uniqueId = uniqueId;
   }
-
   public void setParent(files parent) {
     this.parent = parent;
   }
@@ -29,14 +31,12 @@ class document extends files{
 }
 
 class folder extends files{
-
   List<files> children = new ArrayList<>();
 
   public folder(String name, int uniqueId) {
     super(name, uniqueId);
     isfolder = true;
   }
-
   public String[] childrenNames(){
     String[] nameList = new String[children.size()];
     for(int i = 0 ;i< children.size();i++){
@@ -57,7 +57,7 @@ class SigmaFileSystem {
 
   public SigmaFileSystem() {
     filesystem = new HashMap<>();
-    root = new folder("MyDocuments",0);
+    root = new folder("MyDocuments",0);  //assumption root is mydocuments and 0 as uniqueid
     filesystem.put(0,root);
   }
 
@@ -69,33 +69,36 @@ class SigmaFileSystem {
     return totalWorksheets;
   }
   // I added the uniqueId parameter because a new file needs a uniqueid for itself
+  //assumption: the filetype is either worksheet, dashboard, or folder
+  //assumption: the folderId exists
   void addNewFile(String fileName, String fileType,int uniqueId, int folderId) {
     possibleDepth++;
     if(fileType.equals("worksheet")){
-      document newdocument = new document(fileName,uniqueId,'w');
+      document newDocument = new document(fileName,uniqueId,'w');
       folder folderlocation = (folder) filesystem.get(folderId);
-      newdocument.setParent(folderlocation);
-      filesystem.put(uniqueId,newdocument);
-      folderlocation.children.add(newdocument);
+      newDocument.setParent(folderlocation);
+      filesystem.put(uniqueId,newDocument);
+      folderlocation.children.add(newDocument);
       totalWorksheets++;
     }
     if(fileType.equals("dashboard")){
-      document newdocument = new document(fileName,uniqueId,'d');
+      document newDocument = new document(fileName,uniqueId,'d');
       folder folderlocation = (folder) filesystem.get(folderId);
-      newdocument.setParent(folderlocation);
-      filesystem.put(uniqueId,newdocument);
-      folderlocation.children.add(newdocument);
+      newDocument.setParent(folderlocation);
+      filesystem.put(uniqueId,newDocument);
+      folderlocation.children.add(newDocument);
       totalDashboards++;
     }
     if(fileType.equals("folder")){
-      folder newfolder = new folder(fileName,uniqueId);
+      folder newFolder = new folder(fileName,uniqueId);
       folder folderlocation = (folder) filesystem.get(folderId);
-      newfolder.setParent(folderlocation);
-      filesystem.put(uniqueId,newfolder);
-      folderlocation.children.add(newfolder);
+      newFolder.setParent(folderlocation);
+      filesystem.put(uniqueId,newFolder);
+      folderlocation.children.add(newFolder);
     }
   }
-
+  //assumption: folderId exists and the fileName exists in that folder
+  //allows for duplicate filenames but in different/unique folders
   int getFileId(String fileName, int folderId) {
     int fileid = -1;
     if(fileName.equals("MyDocuments")){ //although the instruction said not required, I assumed 0 for MyDocument
@@ -111,9 +114,11 @@ class SigmaFileSystem {
     return fileid;
   }
 
+  //assumption: fileId exists
+  //assumption: newfolderId exists
   void moveFile(int fileId, int newFolderId) {
     files temp = filesystem.get(fileId);
-    folder folderlocation = (folder) temp.parent;
+    folder folderlocation = (folder) temp.parent; //go into the folder which is the parent
     folderlocation.children.remove(temp);
     filesystem.remove(fileId);
     folder newfolderlocation = (folder) filesystem.get(newFolderId);
@@ -121,55 +126,111 @@ class SigmaFileSystem {
     filesystem.put(fileId,temp);
   }
 
+  //assumption: folderId exists
   String[] getFiles(int folderId) {
     folder folderlocation = (folder) filesystem.get(folderId);
     return folderlocation.childrenNames();
   }
 
-  void printTree(files file, int depth, boolean[] flag, boolean isLast){
+  //helper recursive function
+  void printTree(files file, int depth, boolean isLast){
     for (int i = 1; i < depth; ++i) {
-      if (flag[i]) {
-        System.out.print("| " + " " + " " + " ");
-      }
-      else {
-        System.out.print(" " + " " + " " + " ");
-      }
+      System.out.print("│     ");
     }
-
     if(depth == 0){
-      System.out.println(file.name);
+      System.out.println(file.name+"("+file.uniqueId+")");
     }
-
     else if (isLast) {
-      System.out.print("+--- " +  file.name + '\n');
-      flag[depth] = false;
+      System.out.print("├─── " +  file.name+"("+file.uniqueId+")" + '\n');
     }
     else {
-      System.out.print("+--- " +  file.name + '\n');
+      System.out.print("├─── " +  file.name+"("+file.uniqueId+")" + '\n');
     }
-
     int it = 0;
+    //traverse to its children
     if(file.isfolder){
       folder folder = (folder) file;
       for (files f : folder.children) {
         ++it;
-        printTree(f, depth + 1, flag, it == (folder.children.size()) - 1);
+        printTree(f, depth + 1, it == (folder.children.size()) - 1);
       }
     }
-    flag[depth] = true;
   }
 
+  //assumption: print file structure by its name and uniqueId
   void printFiles() {
-    boolean[] flag = new boolean[possibleDepth];
-    Arrays.fill(flag, true);
-    printTree(root, 0,flag, false);
+    printTree(root, 0, false);
   }
 
 }
-//TESTING SEQUENCE
-public class SigmaFile {
+
+/////////////////////////////////////////////////////////
+// YOU DO NOT NEED TO MAKE CHANGES BELOW UNLESS NECESSARY
+/////////////////////////////////////////////////////////
+
+/*
+ * Note from Coder Pad: To execute Java, please define "static void main" on a class
+ * named Solution.
+ *
+ * If you need more classes, simply define them inline.
+ */
+class Solution {
+
+  private static int askForInteger(Scanner scanner, String question) {
+    System.out.println(question);
+    try {
+      return Integer.parseInt(scanner.nextLine());
+    } catch (Exception e) {
+      System.out.println("Please enter a valid integer.");
+      return askForInteger(scanner, question);
+    }
+  }
+
+  public static void runExample() {
+
+    // compared output against expected output
+    SigmaFileSystem fs = new SigmaFileSystem();
+    int rootId = fs.getFileId("MyDocuments", 0);
+    fs.addNewFile("draft", "folder", 1, rootId);           //i added a paramter of the unique id because i noticed that there wasn't a paarameter for it
+    fs.addNewFile("complete", "folder", 2, rootId);    //added unique id parameter
+    int draftId = fs.getFileId("draft", rootId);  //1
+    int completeId = fs.getFileId("complete", rootId);  //2
+    fs.addNewFile("foo", "worksheet", 3, draftId);                 //added unique id parameter
+    fs.addNewFile("bar", "dashboard", 4, completeId);              //added unique id parameter
+    int fooId = fs.getFileId("foo", draftId);  //3
+    fs.moveFile(fooId, completeId);
+
+    System.out.println(String.join(", ", fs.getFiles(rootId)));  //draft,complete
+    System.out.println(String.join(", ", fs.getFiles(draftId))); // (nothing)
+    System.out.println(String.join(", ", fs.getFiles(completeId)));  //bar,foo
+
+    fs.addNewFile("project", "folder",9,  draftId);                  //added unique id parameter
+    int projectId = fs.getFileId("project", draftId);              //9
+    fs.addNewFile("page1", "worksheet", 5, projectId);              //added unique id parameter
+    fs.addNewFile("page2", "worksheet", 6, projectId);               //added unique id parameter
+    fs.addNewFile("page3", "worksheet", 7,  projectId);                  //added unique id parameter
+    fs.addNewFile("cover", "dashboard", 8, projectId);              //added unique id parameter
+    fs.moveFile(projectId, completeId);
+    projectId = fs.getFileId("project", completeId); //9
+    int coverId = fs.getFileId("cover", projectId);   //8
+    fs.moveFile(coverId, rootId);
+
+    System.out.println(String.join(", ", fs.getFiles(rootId)));  //draft,complete, cover
+    System.out.println(String.join(", ", fs.getFiles(draftId))); //nothing
+    System.out.println(String.join(", ", fs.getFiles(completeId))); //bar, foo, project
+    System.out.println(String.join(", ", fs.getFiles(projectId))); // page1, page2, page 3
+
+    System.out.println(fs.getTotalDashboards());  //2
+    System.out.println(fs.getTotalWorksheets());  //4
+    fs.printFiles();
+  }
+
+  // Feel free to modify this main function as you see fit.
   public static void main(String[] args) {
+
+  //TESTING SEQUENCE
     /*
+    //I tested my functinons by testing for the correctness of the functions after a certain input and seeing if the result matched the expectation, to which it will return PASS. If it does not match the expectation, it will return false. I also tested the run_example function and matched it with my drawings on paper as I followed along to ensure the correctness of the function.
     SigmaFileSystem fs =new SigmaFileSystem();
     if(fs.getFileId("MyDocuments",0)==0){
       System.out.println("PASS");
@@ -205,7 +266,7 @@ public class SigmaFile {
     }
     fs.printFiles();
     fs.addNewFile("katie","folder",4, 0);
-    if(fs.getFileId("katie",0)==3){
+    if(fs.getFileId("katie",0)==4){
       System.out.println("PASS");
     } else{
       System.out.println("FAIL");
@@ -222,62 +283,31 @@ public class SigmaFile {
     fs.addNewFile("computer","dashboard",6,5);
     fs.moveFile(5,1);
     fs.addNewFile("laptop","worksheet",7,5);
-    fs.addNewFile("erin","worksheet",8,4);
+    fs.addNewFile("erin","worksheet",8,4);  //test for duplicate name but different file(in different folder)
+    fs.addNewFile("dog","worksheet",9,1);
+    fs.addNewFile("cat","folder",10,5);
+    fs.addNewFile("basektball","dashboard",11,10);
+    fs.addNewFile("ipad","dashboard",12,4);
     fs.printFiles();
-
-    if(fs.getTotalDashboards()==2){
+    if(fs.getTotalDashboards()==4){
       System.out.println("PASS");
     } else{
       System.out.println("FAIL");
     }
-    if(fs.getTotalWorksheets()==2){
+    if(fs.getTotalWorksheets()==4){
       System.out.println("PASS");
     } else{
       System.out.println("FAIL");
     }
 */
-    SigmaFileSystem fs = new SigmaFileSystem();
-    int rootId = fs.getFileId("MyDocuments", 0);
-    System.out.println(rootId);
-    fs.addNewFile("draft", "folder", 1, rootId);            //i added a paramter of the unique id because i noticed that there wasn't a paarameter for it
-    fs.addNewFile("complete", "folder", 2, rootId);
-    int draftId = fs.getFileId("draft", rootId);
-    int completeId = fs.getFileId("complete", rootId);
-    fs.addNewFile("foo", "worksheet", 3, draftId);
-    fs.addNewFile("bar", "dashboard", 4, completeId);
-    int fooId = fs.getFileId("foo", draftId);
-    fs.moveFile(fooId, completeId);
 
-    System.out.println(String.join(", ", fs.getFiles(rootId)));
-    System.out.println(String.join(", ", fs.getFiles(draftId)));
-    System.out.println(String.join(", ", fs.getFiles(completeId)));
 
-    fs.addNewFile("project", "folder",9,  draftId);
-    int projectId = fs.getFileId("project", draftId);
-    fs.addNewFile("page1", "worksheet", 5, projectId);
-    fs.addNewFile("page2", "worksheet", 6, projectId);
-    fs.addNewFile("page3", "worksheet", 7,  projectId);
-    fs.addNewFile("cover", "dashboard", 8, projectId);
-    fs.moveFile(projectId, completeId);
-    projectId = fs.getFileId("project", completeId);
-    int coverId = fs.getFileId("cover", projectId);
-    fs.moveFile(coverId, rootId);
-
-    System.out.println(String.join(", ", fs.getFiles(rootId)));
-    System.out.println(String.join(", ", fs.getFiles(draftId)));
-    System.out.println(String.join(", ", fs.getFiles(completeId)));
-    System.out.println(String.join(", ", fs.getFiles(projectId)));
-
-    System.out.println(fs.getTotalDashboards());
-    System.out.println(fs.getTotalWorksheets());
-    fs.printFiles();
-/*
     boolean running = true;
     Scanner scanner = new Scanner(System.in);
     SigmaFileSystem fs = new SigmaFileSystem();
     int command;
     while (running) {
-      command = askForInteger(scanner, "\nEnter an integer to indicate a command: \n[1] get_total_dashboards\n[2] get_total_worksheets\n[3] add_new_folder\n[4] get_file_id\n[5] move_file\n[6] get_files \n[7] print_files\n[8] exit\n");
+      command = askForInteger(scanner, "\nEnter an integer to indicate a command: \n[1] get_total_dashboards\n[2] get_total_worksheets\n[3] add_new_file\n[4] get_file_id\n[5] move_file\n[6] get_files \n[7] print_files\n[8] exit\n");         //I changed it to from add new folder to add new file, because the function should be able to add any file
       switch (command) {
         case 1: {
           int totalDashboards = fs.getTotalDashboards();
@@ -294,8 +324,9 @@ public class SigmaFile {
           String fileName = scanner.nextLine();
           System.out.println("Enter a file type (worksheet, dashboard, or folder)");
           String fileType = scanner.nextLine();
+          int uniqueId = askForInteger(scanner, "What would be the unique id for the file?");     //added the unique id ask to fill the parameter
           int folderId = askForInteger(scanner, "Enter a folder id where you'd like to put this file");
-          fs.addNewFile(fileName, fileType, folderId);
+          fs.addNewFile(fileName, fileType, uniqueId, folderId);
           System.out.println(String.format("%s has been added to folder %d", fileName, folderId));
           break;
         }
@@ -341,6 +372,5 @@ public class SigmaFile {
           System.out.println(String.format("Invalid command: %d. Please try again.\n",command));
       }
     }
-    */
   }
 }
